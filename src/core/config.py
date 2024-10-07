4c7949fd7e499f34897053795bf364ef1cf4bf6c
GennadyBr <gennady.budazhapov@gmail.com>
2024-10-07 14:54:24 +0300

    del _mk_log_dir

--- src/core/config.py
+++ src/core/config.py
@@ -1,15 +1,11 @@
 """ Config """
 import os
 
-
-def _mk_log_dir(folder: str) -> None:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(cur_dir)
 log_config_filename = f'{cur_dir}/log_conf.yaml'
 
-_mk_log_dir(f'{parent_dir}/logs')
+if not os.path.exists(f'{parent_dir}/logs'):
+    os.makedirs(f'{parent_dir}/logs')
+
 log_rotation_filename = f'{parent_dir}/logs/logs.log'
